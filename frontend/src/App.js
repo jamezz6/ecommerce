e9a0bd0e0e479a6b47ff9d17783f5a51775499ca
jessblob <jessicachau370@gmail.com>
2023-09-13 03:08:54 -0700

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [meal, setMeal] = useState({});
+
+  const handler = async () => {
+    try {
+      const response = await fetch("www.themealdb.com/api/json/v1/1/search.php?f=b");
+      const data = await response.json();
+      setMeal(data.meals[0]);
+    } catch (error) {
+      console.error("Error fetching meal:", error);
+    }
+  };
+
+  useEffect(() => {
+    handler();
+  }, []);
+
+  return (<body>
+    <div class = "whole">
+      <div class = "container">
+      <h1 class = "title">Random Meals</h1>
+     
+      <button class = "button" onClick={handler}>Click</button>
+      </div>
+      <div class = "meal">
+        <h2 >{meal.strMeal}</h2>
+        <h2>CATEGORY: {meal.strCategory}</h2>
+        <h2>AREA: {meal.strArea}</h2>
+        <p>INSTRUCTIONS: <p></p> {meal.strInstructions}</p>
+        <img  src={meal.strMealThumb} alt={meal.strMeal} />
+      </div>
     </div>
+    </body>
   );
-}
+};
 
 export default App;
